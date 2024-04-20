62d594e170e781db6656b6b82e440f540a6fd68a
tarun-jaat <badgujjar9991@gmail.com>
2024-04-20 11:30:17 +0530

    first commit

---
+++ js.js
@@ -0,0 +1,11 @@
+
+function sideMenu(side) {
+  var menu = document.getElementById('side-menu');
+  if(side==0) {
+    menu.style = 'transform: translatey(0vh); position:fixed;';
+  }
+  else {
+    menu.style = 'transform: translateX(-100%);';
+  }
+  side++;
+}
